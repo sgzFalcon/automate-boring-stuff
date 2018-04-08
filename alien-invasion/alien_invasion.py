24d5efce735fd7b2dbddc557971b89852a9c1a32
Sergio Falcon <sgzfalcon@gmail.com>
2018-04-08 17:58:56 +0200

    Add aliens and collisions'

--- alien-invasion/alien_invasion.py
+++ alien-invasion/alien_invasion.py
@@ -15,12 +15,15 @@ def run_game():
 
     ship = Ship(ai_settings, screen)
     bullets = Group()
+    aliens = Group()
+
+    gf.create_fleet(ai_settings, screen, ship, aliens)
     #Start the main loop for the game
     while True:
         gf.check_events(ai_settings,screen, ship, bullets)
         ship.update()
-        gf.update_bullets(bullets)
-
-        gf.update_screen(ai_settings,screen,ship, bullets)
+        gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
+        gf.update_aliens(ai_settings, aliens)
+        gf.update_screen(ai_settings,screen,ship, aliens, bullets)
 
 run_game()
